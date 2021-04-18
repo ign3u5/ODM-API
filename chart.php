9fa439441fd6bf4532c5d109461d660de6128dc4
Chris <chrisfarmer19801@gmail.com>
2021-04-18 16:16:33 +0100

    changed CORS headers

--- chart.php
+++ chart.php
@@ -1,10 +1,9 @@
 <?php
     header("Access-Control-Allow-Origin: *");   
     header("Content-Type: application/json; charset=UTF-8");    
-    header("Access-Control-Allow-Methods: GET, PUT, OPTIONS");    
+    header("Access-Control-Allow-Methods: GET, OPTIONS");    
     header("Access-Control-Max-Age: 3600");    
-    header("Access-Control-Allow-Headers: Content-Type, content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Token");  
-    header("Access-Control-Expose-Headers: Token");
+    header("Access-Control-Allow-Headers: Content-Type, content-type, Access-Control-Allow-Headers, Authorization, X-Requested-With");  
     require_once "orchestrators/ChartMethodOrchestrator.php";
     require_once "handlers/ResponseHandler.php";
     
